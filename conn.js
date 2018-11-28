edcf64402ea14b62bb927149d2731758a9ec505f
Mariam Domínguez <maryanpopis@hotmail.com>
2018-11-28 16:21:19 -0600

    change from mariadb to mysql

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 // Start a connection with mysql
 const db = mysql.createConnection({
-  host: "robogram.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
-  user: "masteruser",
+  host: "robogramdb.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
+  user: "masteradmin",
   password: "FvK9TKtqC2tqHHec",
-  database: "robogram",
+  database: "robogramdb",
   port: 3306,
   connectTimeout: 30000
 });
