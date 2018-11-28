6945b73bbe3a1e3c707550cb163758e5c970e1c4
Mariam Domínguez <maryanpopis@hotmail.com>
2018-11-28 16:48:43 -0600

    try pooling

--- conn.js
+++ conn.js
@@ -1,22 +1,32 @@
 const mysql = require("mysql2");
 
 // Start a connection with mysql
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: "robogramdb.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
+//   user: "masteradmin",
+//   password: "FvK9TKtqC2tqHHec",
+//   database: "robogramdb",
+//   port: 3306,
+//   connectTimeout: 30000
+// });
+
+// db.connect(function(err) {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   } else {
+//     console.log("database is connected...");
+//   }
+// });
+
+const pool = mysql.createPool({
   host: "robogramdb.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
   user: "masteradmin",
   password: "FvK9TKtqC2tqHHec",
   database: "robogramdb",
-  port: 3306,
-  connectTimeout: 30000
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(function(err) {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    console.log("database is connected...");
-  }
-});
-
-module.exports = db;
+module.exports = pool;
