cedbbd5c7cb0ebdc71db1d472b6e8f8e363a8c36
Mariam Domínguez <maryanpopis@hotmail.com>
2018-11-28 18:43:09 -0600

    enviroment variables

--- conn.js
+++ conn.js
@@ -1,29 +1,11 @@
 const mysql = require("mysql2");
 
-// Start a connection with mysql
-// const db = mysql.createConnection({
-//   host: "robogramdb.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
-//   user: "masteradmin",
-//   password: "FvK9TKtqC2tqHHec",
-//   database: "robogramdb",
-//   port: 3306,
-//   connectTimeout: 30000
-// });
-
-// db.connect(function(err) {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   } else {
-//     console.log("database is connected...");
-//   }
-// });
-
 const pool = mysql.createPool({
-  host: "robogramdb.ckcm8ttzwplv.us-west-2.rds.amazonaws.com",
-  user: "masteradmin",
-  password: "FvK9TKtqC2tqHHec",
-  database: "robogramdb",
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
